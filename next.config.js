9570a00a559211f21711444c20f65f3e36299ce0
Riki Wendri <fayyadh.farizzi@gmail.com>
2024-01-13 14:47:24 +0700

    edit next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
+  },
+  images: {
+    domains: [],
+    // Add the remote patterns for your images
+    loader: 'default',
+    path: '/_next/image',
+    disableStaticImages: true,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        hostname: 'localhost',
+        protocol: 'http', // or 'https' if applicable
+      },
+    ],
+    minimumCacheTTL: 60,
   }
 }
 
