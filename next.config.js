780eadc30a9986975b9c4e96ed1b915be0bc64cf
Riki Wendri <fayyadh.farizzi@gmail.com>
2024-01-13 15:57:18 +0700

    edit next.config

--- next.config.js
+++ next.config.js
@@ -4,22 +4,39 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  images: {
-    domains: [],
-    // Add the remote patterns for your images
-    loader: 'default',
-    path: '/_next/image',
-    disableStaticImages: true,
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    remotePatterns: [
-      {
-        hostname: 'localhost',
-        protocol: 'http', // or 'https' if applicable
-      },
-    ],
-    minimumCacheTTL: 60,
-  }
+  // images: {
+  //   domains: [],
+  //   // Add the remote patterns for your images
+  //   loader: 'default',
+  //   path: '/_next/image',
+  //   disableStaticImages: true,
+  //   deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  //   imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  //   remotePatterns: [
+  //     {
+  //       hostname: 'localhost',
+  //       protocol: 'http', // or 'https' if applicable
+  //     },
+  //   ],
+  //   minimumCacheTTL: 60,
+  // },
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.(png|jpe?g|gif|svg)$/i,
+  //     use: [
+  //       {
+  //         loader: 'file-loader',
+  //         options: {
+  //           name: '[name].[ext]',
+  //           publicPath: '/_next/static/images',
+  //           outputPath: 'static/images',
+  //         },
+  //       },
+  //     ],
+  //   });
+
+  //   return config;
+  // }
 }
 
 module.exports = nextConfig
