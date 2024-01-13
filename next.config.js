8f8d4e7d895d9211b3dd53a674c1bf4efdb2b378
Riki Wendri <fayyadh.farizzi@gmail.com>
2024-01-13 16:21:40 +0700

    edit next.config disableStaticImages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    disableStaticImages: true,
+  },
   // images: {
   //   domains: [],
   //   // Add the remote patterns for your images
