cdcefac6d3c4454828f057887f84ae445bac4769
Riki Wendri <fayyadh.farizzi@gmail.com>
2024-01-18 10:03:05 +0700

    inactivated remotePattern, headers, rewrites in next.config

--- next.config.js
+++ next.config.js
@@ -6,45 +6,45 @@ const nextConfig = {
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'faysdalgona.netlify.app',
-        port: '',
-        pathname: '/app/images/**',
-      }
-    ]
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'faysdalgona.netlify.app',
+    //     port: '',
+    //     pathname: '/app/images/**',
+    //   }
+    // ]
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // Allow any route
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*', // Allow any origin (you might want to restrict this in production)
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Origin, X-Requested-With, Content-Type, Accept',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)', // Allow any route
+  //       headers: [
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: '*', // Allow any origin (you might want to restrict this in production)
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: 'Origin, X-Requested-With, Content-Type, Accept',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 
-  rewrites: async () => ({
-    beforeFiles: [
-      {
-        source: '/(.*)',
-        destination: '/',
-      },
-    ],
-  }),
+  // rewrites: async () => ({
+  //   beforeFiles: [
+  //     {
+  //       source: '/(.*)',
+  //       destination: '/',
+  //     },
+  //   ],
+  // }),
   generateStaticParams: async function () {
     return {
       '/': { page: '/' },
